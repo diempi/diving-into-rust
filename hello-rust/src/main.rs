cf53a67537bddc3efa718754def0aa6ef73968f9
diempi <1102885+diempi@users.noreply.github.com>
2025-12-02 12:42:06 +0100

    Declaring values - integers

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+
+// Variables
+    let x = 5;
+    println!("x is {x}");
+    let mut y = 6;
+    println!("Current value of Y is {y}");
+    y = 9;
+    println!("New value of Y is {y}");
+
+    let z: i32 = 10;
+    println!("Z is a integer and its value is {z}");
+
 }
