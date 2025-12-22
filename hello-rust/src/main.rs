c7fd3b756b0670ec6f9cf05b54a42b8b95341063
diempi <1102885+diempi@users.noreply.github.com>
2025-12-22 12:33:48 +0100

    Creating examples folder

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
     println!("Hello, world! again");
 
-// Variables
+    // Variables
     let x = 5;
     println!("x is {x}");
     let mut y = 6;
@@ -13,11 +13,11 @@ fn main() {
     let z: i32 = 10;
     println!("Z is a integer and its value is {z}");
 
-// Floats
+    // Floats
 
     let f = 3.14;
     println!("fl is a float and its value is {f}");
-// Slices
+    // Slices
     let s = String::from("Hello world");
     let hello = &s[0..5];
     println!("hello is a slice of s and its value is {hello}");
@@ -25,35 +25,56 @@ fn main() {
     let world = &s[6..11];
     println!("world is a slice of s and its value is {world}");
 
-// Vectors
+    // Vectors
 
-    let mut v = vec![1,2,3];
+    let mut v = vec![1, 2, 3];
     println!("Vector v is a vector and its actual value is {:?}", v);
 
     // push to the vector
     v.push(4);
-    println!("Vector v is a vector and its new value is {:?}",v);
+    println!("Vector v is a vector and its new value is {:?}", v);
 
     v.push(5);
 
-   println!("adding to the Vector v is a vector and its new value is {:?}",v);
+    println!(
+        "adding to the Vector v is a vector and its new value is {:?}",
+        v
+    );
 
     // Remove from vector
 
     v.pop();
 
-    println!("removing from the Vector v is a vector and its new value is {:?}",v);
+    println!(
+        "removing from the Vector v is a vector and its new value is {:?}",
+        v
+    );
 
     // Iterators
 
-    let vec = vec![1,2,3,4,5];
+    let vec = vec![1, 2, 3, 4, 5];
     let mut iter = vec.iter();
 
-    println!("the first value of the iterator is {}",iter.next().unwrap());
-    println!("the first value of the iterator is {}",iter.next().unwrap());
-    println!("the first value of the iterator is {}",iter.next().unwrap());
-    println!("the first value of the iterator is {}",iter.next().unwrap());
-    println!("the first value of the iterator is {}",iter.next().unwrap());
+    println!(
+        "the first value of the iterator is {}",
+        iter.next().unwrap()
+    );
+    println!(
+        "the first value of the iterator is {}",
+        iter.next().unwrap()
+    );
+    println!(
+        "the first value of the iterator is {}",
+        iter.next().unwrap()
+    );
+    println!(
+        "the first value of the iterator is {}",
+        iter.next().unwrap()
+    );
+    println!(
+        "the first value of the iterator is {}",
+        iter.next().unwrap()
+    );
     //println!("the first value of the iterator is {}",iter.next().unwrap());
 
     // Functions
@@ -62,9 +83,8 @@ fn main() {
         a + b
     }
 
-    let result = add(3,5);
+    let result = add(3, 5);
     println!("the result of the addition is {result}");
-
 
     // Conditional Flow
 
@@ -76,5 +96,4 @@ fn main() {
     } else {
         println!("number is equal to 5");
     }
-
 }
